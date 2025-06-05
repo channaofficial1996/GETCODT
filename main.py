4e051bf317355a481e90e31f78544c36f29bce1b
channaofficial1996 <channaofficial1996@gmail.com>
2025-06-05 11:47:20 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -151,7 +151,8 @@ async def handle_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 continue
         await q.message.reply_text("❌ No OTP found for alias.")
 
-BOT_TOKEN = "7915387166:AAFeGRGme39-znPBxDLOu8BrHheqsOWUIR4"
+# ✅ Replace with your token
+BOT_TOKEN = "8042421392:AAHMz2z5EJxenhDryF3rAVmMwWN58BbSljs"
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
